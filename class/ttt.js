a65a85630cc7861e6e5ba89ca9e728155adab20f
Kevin <kevindbaik@gmail.com>
2023-06-09 19:46:11 -0700

    created TTT

--- class/ttt.js
+++ class/ttt.js
@@ -1,11 +1,20 @@
+const Cursor = require('./cursor');
 const Screen = require('./screen')
 
 class TTT {
 
   constructor(){
     this.playerTurn = "O";
-    this.grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    this.grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+    this.cursor = new Cursor();
+    Screen.initialized = true;
+    Screen.gridLines = true;
+    Screen.message = "Player O's turn";
+    Screen.backgroundColors = "\u001b[43m";
+    // Screen.backgroundColors.cursor.col = "\u001b[43m";
   }
+
+
 
 }
 
